7e1753ccf051c23fd7bee25c73d90f7ede92a51f
Ángel Emilio Serna Navarro <50151743+Zelvax42@users.noreply.github.com>
2021-10-15 09:45:18 -0700

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        // código basura
+        int _, __, ___ = new int();
+        // Console.WriteLine(${_} {__} {___});
+
+        public static void fibonacci(int a, int b, int i, int limite)
+        {
+            if (i < limite)
+            {
+                if (a < 2)
+                {
+                    Console.WriteLine(a);
+                }
+                Console.WriteLine(b);
+                i++;
+                fibonacci(b, a+b, i, limite);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Escribe el límite de la secuencia Fibonacci");
+            fibonacci(1, 2, 0, int.Parse(Console.ReadLine()));
+            Console.WriteLine("\n");
+        }
+    }
+}
